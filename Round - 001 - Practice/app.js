70f38015ffe1cf2d8d6db12f113412ee5c41d688
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-04-09 23:56:02 +0530

    Practicing Machine Coding Round Probs

---
+++ Round - 001 - Practice/app.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const Heading = () => {
+    return <h1>Rishabh</h1>
+}
+
+const root = ReactDOM.createRoot(document.querySelector(".root"));
+root.render(<Heading />)
