771cc4a25209df301ad49cff8d13cc3d988567af
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-06-16 00:21:03 +0530

    added few more books

--- Round - 001 - Practice/src/constant.js
+++ Round - 001 - Practice/src/constant.js
@@ -24,6 +24,104 @@ export const BookData = [
       "rating":4.5,
       "price": 230,
       "status": ""
-    }
+    },
+    {
+      "bookIsbn": "380728273",
+      "title": "Liberty Falling (Anna Pigeon Mysteries (Paperback))",
+      "author": "Nevada Barr",
+      "yearOfPublication": 2000,
+      "publisher": "Avon",
+      "bookImage": "http://images.amazon.com/images/P/0380728273.01.LZZZZZZZ.jpg",
+      "ratings": 0,
+      "price": 13.99,
+      "description": "Mystery novel by Nevada Barr."
+    },
+    {
+      "bookIsbn": "8433914545",
+      "title": "El Palacio de La Luna",
+      "author": "Paul Auster",
+      "yearOfPublication": 1996,
+      "publisher": "Anagrama",
+      "bookImage": "http://images.amazon.com/images/P/8433914545.01.LZZZZZZZ.jpg",
+      "ratings": 0,
+      "price": 12.49,
+      "description": "Novel by Paul Auster."
+    },
+    {
+      "bookIsbn": "446606197",
+      "title": "Killer Market (Deborah Knott Mysteries (Paperback))",
+      "author": "Margaret Maron",
+      "yearOfPublication": 1999,
+      "publisher": "Warner Books",
+      "bookImage": "http://images.amazon.com/images/P/0446606197.01.LZZZZZZZ.jpg",
+      "ratings": 0,
+      "price": 10.99,
+      "description": "Mystery novel by Margaret Maron."
+    },
+    {
+      "bookIsbn": "771076002",
+      "title": "Remembering Peter Gzowski : A Book of Tributes",
+      "author": "EDNA BARKER",
+      "yearOfPublication": 2002,
+      "publisher": "Douglas Gibson Books",
+      "bookImage": "http://images.amazon.com/images/P/0771076002.01.LZZZZZZZ.jpg",
+      "ratings": 0,
+      "price": 9.99,
+      "description": "Tribute book by Edna Barker."
+    },
+    {
+      "bookIsbn": "771088191",
+      "title": "The Best Canadian Animal Stories: Classic Tales by Master Storytellers",
+      "author": "Muriel Whitaker",
+      "yearOfPublication": 1997,
+      "publisher": "McClelland & Stewart",
+      "bookImage": "http://images.amazon.com/images/P/0771088191.01.LZZZZZZZ.jpg",
+      "ratings": 0,
+      "price": 14.99,
+      "description": "Anthology edited by Muriel Whitaker."
+    },
+    {
+      "bookIsbn": "1559703237",
+      "title": "Trying to Save Piggy Sneed",
+      "author": "John Irving",
+      "yearOfPublication": 1996,
+      "publisher": "Arcade Publishing",
+      "bookImage": "http://images.amazon.com/images/P/1559703237.01.LZZZZZZZ.jpg",
+      "ratings": 0,
+      "price": 11.49,
+      "description": "Short story collection by John Irving."
+    },
+    {
+      "bookIsbn": "3596214629",
+      "title": "Herr Der Fliegen (Fiction, Poetry and Drama)",
+      "author": "Golding",
+      "yearOfPublication": 1954,
+      "publisher": "Fischer Taschenbuch Verlag GmbH",
+      "bookImage": "http://images.amazon.com/images/P/3596214629.01.LZZZZZZZ.jpg",
+      "ratings": 0,
+      "price": 9.99,
+      "description": "Novel by William Golding."
+    },
+    {
+      "bookIsbn": "671847546",
+      "title": "REAL GUIDE: CALIFORNIA AND THE WEST COAST (The Real guides)",
+      "author": "LTD",
+      "yearOfPublication": 1993,
+      "publisher": "John Wiley & Sons Inc",
+      "bookImage": "http://images.amazon.com/images/P/0671847546.01.LZZZZZZZ.jpg",
+      "ratings": 0,
+      "price": 13.99,
+      "description": "Travel guidebook."
+    },
+    {
+      "bookIsbn": "60188731",
+      "title": "Bel Canto",
+      "author": "Ann Patchett",
+      "yearOfPublication": 2001,
+      "publisher": "HarperCollins Publishers",
+      "bookImage": "http://images.amazon.com/images/P/0060188731.01.LZZZZZZZ.jpg",
+      "ratings": 0,
+      "price": 12.99,
+      "description": "Novel by Ann Patchett."
+    }    
 ]
-
