63888714f58d723fb54e1f767616586217d1e27c
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-06-15 17:24:57 +0530

    managed folder structute and added skeleton of Round 1

--- Round - 001 - Practice/app.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-
-const Heading = () => {
-    return <h1>Rishabh</h1>
-}
-
-const root = ReactDOM.createRoot(document.querySelector(".root"));
-root.render(<Heading />)
---
+++ Round - 001 - Practice/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+const App = () => {
+    return (
+        <h1>Rishabh</h1>
+    )
+}
+export default App;
